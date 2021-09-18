af3824fc847cc0cf71bb7f8a16e43d9b3f7d9714
Молоствов Игорь <Jaiko876@gmail.com>
2021-09-18 13:06:36 +0300

    Initial commit

---
+++ src/main/java/com/mt_group/chancelogic/ChanceLogicApplication.java
@@ -0,0 +1,13 @@
+package com.mt_group.chancelogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanceLogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChanceLogicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mt_group/chancelogic/ChanceLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt_group.chancelogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanceLogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
